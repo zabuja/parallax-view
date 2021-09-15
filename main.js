a8abd02f58bc26e5c29dad42ec105fbe0d7513b9
zabuja <atanu.tmsl@gmail.com>
2021-09-15 22:07:34 +0530

    almost styling completed, started the functionality part

---
+++ main.js
@@ -0,0 +1,9 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let mountains_front = document.getElementById('mountains_front');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+
+
+
